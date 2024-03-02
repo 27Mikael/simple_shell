125f0aa2b6d58ba5683e2178cf07e3451cac2083
27Mikael <vitalybodgan@gmail.com>
2024-03-02 13:30:26 +0200

    attempted fix for one extra line

--- interactive_mode.c
+++ interactive_mode.c
@@ -6,20 +6,10 @@
 */
 void print_prompt(void)
 {
-	char cwd[PATH_MAX], username[LOGIN_NAME_MAX], hostname[HOST_NAME_MAX];
-
-	getcwd(cwd, sizeof(cwd));
-	getlogin_r(username, sizeof(username));
-	gethostname(hostname, sizeof(hostname));
-
-	write(STDOUT_FILENO, username, strlen(username));
-	write(STDOUT_FILENO, "@", 1);
-	write(STDOUT_FILENO, hostname, strlen(hostname));
-	write(STDOUT_FILENO, ":", 1);
-	write(STDOUT_FILENO, cwd, strlen(cwd));
-	write(STDOUT_FILENO, "$ ", 2);
-	fflush(stdout);
+    write(STDOUT_FILENO, "($)", 3);
+    fflush(stdout);
 }
+
 
 /**
  * execute_command - parses non_interactive mode commands
